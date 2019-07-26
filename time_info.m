aceca07128217797dc3117cd079df33608529923
owoeye kehinde <kehindeowoeye@yahoo.com>
2019-07-26 18:05:52 +0100

    Add files via upload

---
+++ time_info.m
@@ -0,0 +1,246 @@
+
+
+%day1,160321
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 09 31 47];
+t3 = [t1(1:3) 09 32 49];
+t4 = [t1(1) 03 22 1 26 29];
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:);vy = ally1(tt+1:tt1,:);
+
+
+
+%day2,160322
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 15 58 53];
+t3 = [t1(1:3) 16 00 00];
+t4 = [t1(1) 03 23 4 37 00];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:); vy = ally1(tt+1:tt1,:);
+
+
+
+
+%day3, 160324
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 09 20 14];
+t3 = [t1(1:3) 09 21 50];
+t4 = [t1(1) 03 24 20 02 53];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:); vy = ally1(tt+1:tt1,:);
+
+
+%day4,160325
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 15 56 29];
+t3 = [t1(1:3) 15 59 40];
+t4 = [t1(1) 03 26 05 16 33];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:); vy = ally1(tt+1:tt1,:);
+
+
+
+
+%day5,160328
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 09 27 09];
+t3 = [t1(1:3) 09 32 24];
+t4 = [t1(1) 03 28 21 53 00];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:); vy = ally1(tt+1:tt1,:);
+
+
+
+%day6,160329
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 15 43 15];
+t3 = [t1(1:3) 15 47 25];
+t4 = [t1(1) 03 30 08 02 37];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt1,:); vy = ally1(tt+1:tt1,:);
+
+
+%day7, 160401
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 16 03 16];
+t3 = [t1(1:3) 16 10 48];
+t4 = [t1(1) 04 02 08 11 00];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt+tt1,:); vy = ally1(tt+1:tt+tt1,:);
+
+
+
+%day8,160404
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 09 21 23];
+t3 = [t1(1:3) 09 31 20];
+t4 = [t1(1) 04 05 03 02 33];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt+tt1,:); vy = ally1(tt+1:tt+tt1,:);
+
+
+
+%day9,160405
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 15 54 46];
+t3 = [t1(1:3) 16 05 28];
+t4 = [t1(1) 04 06 10 46 02];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt+tt1,:); vy = ally1(tt+1:tt+tt1,:);
+
+
+%day10,160407
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 09 34 16 ];
+t3 = [t1(1:3) 09 44 00];
+t4 = [t1(1) 04 08 2 36 00];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt+tt1,:); vy = ally1(tt+1:tt+tt1,:);
+
+
+
+%day 11, 160408
+t1 = time_matrix(1,:);t1 = double(t1);
+t2 = [t1(1:3) 16 07 09];
+t3 = [t1(1:3) 16 16 23];
+t4 = [t1(1) 04 09 4 33 11];
+t5 = time_matrix(2,:);t2 = double(t2);
+tt0 = etime(t2,t1);
+tt = etime(t3,t2);
+tt1 = etime(t4,t3);
+allx1 = allx;ally1=ally;
+allx1(1:tt0,:) =[];ally1(1:tt0,:)=[];
+herdx = allx1(1:tt,:) ;herdy= ally1(1:tt,:);
+vx = allx1(tt+1:tt+tt1,:); vy = ally1(tt+1:tt+tt1,:);
+
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%160401
+t1 = time_matrix(1,:);t1 = double(t1);
+t1 = [t1(1:3) 16 10 49];d
+t2 = [t1(1:3) 19 54 00];s
+t3 = [t1(1:3) 20 34 00];d
+t4 = [t1(1:3) 20 58 17];s
+t5 = [t1(1:3) 23 06 00];d
+%t2 = [t1(1:3) 23 44 00];s
+t6 = [t1(1) 04 02 01 49 42];s
+t7 = [t1(1) 04 02 03 18 40];d
+t8 = [t1(1) 04 02 05 00 42];s
+t9 = [t1(1) 04 02 06 11 00];d
+t10 = [t1(1) 04 02 07 34 39];s
+t11 = [t1(1) 04 02 08 11 00];
+
+
+%160404
+t1 = [t1(1:3) 09 31 21];diverse
+t2 = [t1(1:3) 09 41 16];sta
+t3 = [t1(1:3) 12 29 00];d
+%t2 = [t1(1:3) 14 52 00];s
+t4 = [t1(1:3) 20 11 00];s
+t5 = [t1(1:3) 21 03 00];d
+t6 = [t1(1:3) 22 16 00];s
+t7 = [t1(1) 04 05 00 54 00];d
+t8 = [t1(1) 04 05 01 55 00];s
+end 
+
+
+
+%160405
+t1 = [t1(1:3) 16 05 29];d
+t2 = [t1(1:3) 20 58 00];s
+t3 = [t1(1:3) 21 43 00];d
+t4 = [t1(1) 04 06 00 06 00];s
+t5 = [t1(1) 04 06 00 47 00];d
+t6 = [t1(1) 04 06 02 19 00];s
+t7 = [t1(1) 04 06 05 31 00];d
+t8 = [t1(1) 04 06  08 57 00];s
+end
+
+
+
+
+%160407
+t1 = [t1(1:3) 09 44 01];d
+t2 = [t1(1:3) 10 36 00];s
+t3 = [t1(1:3) 13 41 00];d
+t4 = [t1(1:3) 19 45 00];s
+t5 = [t1(1:3) 20 07 00];d
+t6 = [t1(1:3) 20 15 00];s
+t7 = [t1(1:3) 21 47 00];d
+t8 = [t1(1:3) 22 38 00];s
+t9 = [t1(1) 04 08 00 51 00];d
+t10 = [t1(1) 04 08 02:05:00];s
+
+end
+
+
+
+
+
+
+
+
